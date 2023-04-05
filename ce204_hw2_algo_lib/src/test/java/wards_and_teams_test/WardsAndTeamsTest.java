b3a1ba9d4fc02dd692c3b08f3312a7e315c748c9
Resul Beser <93214337+resulbeser@users.noreply.github.com>
2023-04-05 23:18:01 +0300

    added

--- ce204_hw2_algo_lib/src/test/java/wards_and_teams_test/WardsAndTeamsTest.java
+++ ce204_hw2_algo_lib/src/test/java/wards_and_teams_test/WardsAndTeamsTest.java
@@ -60,102 +60,7 @@ public class WardsAndTeamsTest {
 	    assertEquals("Empire State Building", address.getBuildingName());
 	  }
 	  
-	  @Test
-	    public void FullName_testGetGivenName() {
-	    	FullName name = new FullName("Ahmet", "Selami", "ŞAHİN");
-	    	assertEquals("Ahmet", name.getGivenName());
-	    }
-	    
-	    @Test
-	    public void FullName_testGetGivenName_2() {
-	    	FullName name = new FullName("Mehmet", "Eren", "ERBİL");
-	    	assertEquals("Mehmet", name.getGivenName());
-	    }
-	    @Test
-	    public void FullName_testGetGivenName_3() {
-	    	FullName name = new FullName("Sibel", "", "Demir");
-	    	assertEquals("Sibel", name.getGivenName());
-	    }
-	    @Test
-	    public void FullName_testGetMiddleName() {
-	    	FullName name = new FullName("Ahmet", "Selami", "ŞAHİN");
-	    	assertEquals("Selami", name.getMiddleName());
-	    }
-	    
-	    @Test
-	    public void FullName_testMiddleName_2() {
-	    	FullName name = new FullName("Mehmet", "Eren", "ERBİL");
-	    	assertEquals("Eren", name.getMiddleName());
-	    }
-	    @Test
-	    public void FullName_testGetMiddleName_3() {
-	    	FullName name = new FullName("Sibel", "", "Demir");
-	    	assertEquals("", name.getMiddleName());
-	    }
-	    @Test
-	    public void FullName_testGetFamilyName() {
-	    	FullName name = new FullName("Ahmet", "Selami", "ŞAHİN");
-	    	assertEquals("ŞAHİN", name.getFamilyName());
-	    }
-	    
-	    @Test
-	    public void FullName_testFamilyName_2() {
-	    	FullName name = new FullName("Mehmet", "Eren", "ERBİL");
-	    	assertEquals("ERBİL", name.getFamilyName());
-	    }
-	    @Test
-	    public void FullName_testGetFamilyName_3() {
-	    	FullName name = new FullName("Sibel", "", "Demir");
-	    	assertEquals("Demir", name.getFamilyName());
-	    }
-	    @Test
-	    public void FullName_testSetFamilyName_1() {
-	    	FullName name = new FullName("Mehmet", "Eren", "ERBİL");
-	        name.setFamilyName("AYDIN");
-	        assertEquals("AYDIN",name.getFamilyName());
-	    }
-	    @Test
-	    public void FullName_testSetFamilyName_2() {
-	    	FullName name = new FullName("Sibel", "", "DEMİR");
-	        name.setFamilyName("ŞİŞMAN");
-	        assertEquals("ŞİŞMAN",name.getFamilyName());
-	    }
-	    @Test
-	    public void FullName_testSetFamilyName_3() {
-	    	FullName name = new FullName("Yagiz", "Sedat", "ŞİMŞEK");
-	        name.setFamilyName("ÇEKİRDEK");
-	        assertEquals("ÇEKİRDEK",name.getFamilyName());
-	    }
-	    @Test
-	    public void FullName_testSetFullName_1() {
-	    	FullName name = new FullName("Mehmet", "Eren", "ERBİL");
-	    	name.setGivenName("Polat");
-	    	name.setMiddleName("Can");
-	    	name.setFamilyName("ALEMDAR");
-	        assertEquals("Polat",name.getGivenName());
-	        assertEquals("Can",name.getMiddleName());
-	        assertEquals("ALEMDAR",name.getFamilyName());
-	    }
-	    @Test
-	    public void FullName_testSetFullName_2() {
-	    	FullName name = new FullName("Ömer", "Can", "Yıldız");
-	    	name.setGivenName("Mahmut");
-	    	name.setMiddleName("Erdem");
-	    	name.setFamilyName("Tuncer");
-	        assertEquals("Mahmut",name.getGivenName());
-	        assertEquals("Erdem",name.getMiddleName());
-	        assertEquals("Tuncer",name.getFamilyName());
-	    }
-	    @Test
-	    public void FullName_testSetFullName_3() {
-	    	FullName name = new FullName("Ali", "Ahmet", "Atasoy");
-	    	name.setGivenName("Sencer");
-	    	name.setMiddleName("Resul");
-	    	name.setFamilyName("Karahanlı");
-	        assertEquals("Sencer",name.getGivenName());
-	        assertEquals("Resul",name.getMiddleName());
-	        assertEquals("Karahanlı",name.getFamilyName());
-	    }
+	  
 
 	    @Test
 	    public void Phone_testGetPhoneNumber_1() {
@@ -241,52 +146,7 @@ public class WardsAndTeamsTest {
 	      assertEquals(address, hospital.getAddress());
 	    }
 	    
-	    @Test
-	    public void Gender_testSetMale_1() {
-	      Gender gender = new Gender("Male", "Female");
-	      gender.setMale("M");
-	      assertEquals("M", gender.getMale());
-	    }
-
-	    @Test
-	    public void Gender_testSetFemale_1() {
-	      Gender gender = new Gender("Male", "Female");
-	      gender.setFemale("F");
-	      assertEquals("F", gender.getFemale());
-	    }
-
-	    @Test
-	    public void Gender_testGetMale_1() {
-	      Gender gender = new Gender("Male", "Female");
-	      assertEquals("Male", gender.getMale());
-	    }
-
-	    @Test
-	    public void Gender_testGetFemale_1() {
-	      Gender gender = new Gender("Male", "Female");
-	      assertEquals("Female", gender.getFemale());
-	    }
-	    @Test
-	    public void Gender_testGetFemale_2() {
-	      Gender gender = new Gender("Man", "Woman");
-	      assertEquals("Woman", gender.getFemale());
-	    }
-	    
-	    @Test
-	    public void Department_testGetHospital_1() {
-	      Hospital hospital = new Hospital("Numune Hast.", new Phone(123456789), "Trabzon", "Ortahisar", "Erdoğdu", "No:5");
-	      Department department = new Department(hospital);
-	      assertEquals(hospital, department.getHospital());
-	    }
-
-	    @Test
-	    public void Department_testSetHospital_1() {
-	      Hospital hospital1 = new Hospital("Numune Hast.", new Phone(123456789), "Trabzon", "Ortahisar", "Erdoğdu", "No:5");
-	      Hospital hospital2 = new Hospital("Haçkalı Hast.", new Phone(123456789), "Trabzon", "Akçaabat", "101. Sok.", "No:8");
-	      Department department = new Department(hospital1);
-	      department.setHospital(hospital2);
-	      assertEquals(hospital2, department.getHospital());
-	    }
+	   
 	    @Test
 	    public void History_testSetSicknesses_1() {
 	      History history = new History("Flu");
