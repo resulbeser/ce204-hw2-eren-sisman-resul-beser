8e6d808cefd3ec6cf4e921f85c83fe6c164f9cfc
ERENSISMAN61 <eren_sisman20@erdogan.edu.tr>
2023-04-05 23:07:39 +0300

    tests added

--- ce204_hw2_algo_lib/src/test/java/treatment_test/TreatmentTest.java
+++ ce204_hw2_algo_lib/src/test/java/treatment_test/TreatmentTest.java
@@ -7,9 +7,64 @@ import treatment.*;
 
 public class TreatmentTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+	  @Test
+	  public void testGetPrescription() {
+	    Patient patient = new Patient();
+	    Prescription prescription = new Prescription(patient);
+	    Therapy therapy = new Therapy(patient, prescription);
+	    assertEquals(prescription, therapy.getPrescription());
+	  }
 
+	  @Test
+	  public void testSetPrescription() {
+	    Patient patient = new Patient();
+	    Prescription prescription = new Prescription(patient);
+	    Therapy therapy = new Therapy(patient, prescription);
+	    Prescription newPrescription = new Prescription(patient);
+	    assertTrue(therapy.setPrescription(newPrescription));
+	    assertEquals(newPrescription, therapy.getPrescription());
+	    assertTrue(newPrescription.getTherapies().contains(therapy));
+	    assertFalse(prescription.getTherapies().contains(therapy));
+	  }
+
+	  @Test
+	  public void testGetPatient() {
+	    Patient patient = new Patient();
+	    Prescription prescription = new Prescription(patient);
+	    Therapy therapy = new Therapy(patient, prescription);
+	    assertEquals(patient, therapy.getPatient());
+	  }
+
+	  @Test
+	  public void testSetPatient() {
+	    Patient patient = new Patient();
+	    Prescription prescription = new Prescription(patient);
+	    Therapy therapy = new Therapy(patient, prescription);
+	    Patient newPatient = new Patient();
+	    assertTrue(therapy.setPatient(newPatient));
+	    assertEquals(newPatient, therapy.getPatient());
+	    assertTrue(newPatient.getTreatments().contains(therapy));
+	    assertFalse(patient.getTreatments().contains(therapy));
+	  }
+
+	  @Test
+	  public void testGetDoctor() {
+	    Patient patient = new Patient();
+	    Doctor doctor = new Doctor();
+	    Examination examination = new Examination(patient, doctor);
+	    assertEquals(doctor, examination.getDoctor());
+	  }
+
+	  @Test
+	  public void testGetTherapy() {
+	    Patient patient = new Patient();
+	    Prescription prescription = new Prescription(patient);
+	    Therapy therapy1 = new Therapy(patient, prescription);
+	    Therapy therapy2 = new Therapy(patient, prescription);
+	    prescription.addTherapy(therapy1);
+	    prescription.addTherapy(therapy2);
+	    assertEquals(therapy1, prescription.getTherapy(0));
+	    assertEquals(therapy2, prescription.getTherapy(1));
+	  }
+	  
 }
--- ce204_hw2_algo_lib/src/test/java/wards_and_teams_test/WardsAndTeamsTest.java
+++ ce204_hw2_algo_lib/src/test/java/wards_and_teams_test/WardsAndTeamsTest.java
@@ -2,13 +2,315 @@ package wards_and_teams_test;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
+
+import organization.Address;
+import organization.Department;
+import organization.FullName;
+import organization.Gender;
+import organization.History;
+import organization.Hospital;
+import organization.Phone;
 import wards_and_teams.*;
 
 public class WardsAndTeamsTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+	  public void Address_testSetCityName_1() {
+	    Address address = new Address("New York", "Manhattan", "5th Ave", "Empire State Building");
+	    address.setCityName("Los Angeles");
+	    assertEquals("Los Angeles", address.getCityName());
+	  }
+
+	  @Test
+	  public void Address_testSetDistrictName() {
+	    Address address = new Address("New York", "Manhattan", "5th Ave", "Empire State Building");
+	    address.setDistrictName("Brooklyn");
+	    assertEquals("Brooklyn", address.getDistrictName());
+	  }
+
+	  @Test
+	  public void Address_testSetStreetName() {
+	    Address address = new Address("New York", "Manhattan", "5th Ave", "Empire State Building");
+	    address.setStreetName("Broadway");
+	    assertEquals("Broadway", address.getStreetName());
+	  }
+
+	  @Test
+	  public void Address_testSetBuildingName() {
+	    Address address = new Address("New York", "Manhattan", "5th Ave", "Empire State Building");
+	    address.setBuildingName("Chrysler Building");
+	    assertEquals("Chrysler Building", address.getBuildingName());
+	  }
+
+	  @Test
+	  public void Address_testGetCityName() {
+	    Address address = new Address("New York", "Manhattan", "5th Ave", "Empire State Building");
+	    assertEquals("New York", address.getCityName());
+	  }
+
+	  @Test
+	  public void Address_testGetDistrictName() {
+	    Address address = new Address("New York", "Manhattan", "5th Ave", "Empire State Building");
+	    assertEquals("Manhattan", address.getDistrictName());
+	  }
+
+	  @Test
+	  public void Address_testGetStreetName() {
+	    Address address = new Address("New York", "Manhattan", "5th Ave", "Empire State Building");
+	    assertEquals("5th Ave", address.getStreetName());
+	  }
+
+	  @Test
+	  public void Address_testGetBuildingName() {
+	    Address address = new Address("New York", "Manhattan", "5th Ave", "Empire State Building");
+	    assertEquals("Empire State Building", address.getBuildingName());
+	  }
+	  
+	  @Test
+	    public void FullName_testGetGivenName() {
+	    	FullName name = new FullName("Ahmet", "Selami", "ŞAHİN");
+	    	assertEquals("Ahmet", name.getGivenName());
+	    }
+	    
+	    @Test
+	    public void FullName_testGetGivenName_2() {
+	    	FullName name = new FullName("Mehmet", "Eren", "ERBİL");
+	    	assertEquals("Mehmet", name.getGivenName());
+	    }
+	    @Test
+	    public void FullName_testGetGivenName_3() {
+	    	FullName name = new FullName("Sibel", "", "Demir");
+	    	assertEquals("Sibel", name.getGivenName());
+	    }
+	    @Test
+	    public void FullName_testGetMiddleName() {
+	    	FullName name = new FullName("Ahmet", "Selami", "ŞAHİN");
+	    	assertEquals("Selami", name.getMiddleName());
+	    }
+	    
+	    @Test
+	    public void FullName_testMiddleName_2() {
+	    	FullName name = new FullName("Mehmet", "Eren", "ERBİL");
+	    	assertEquals("Eren", name.getMiddleName());
+	    }
+	    @Test
+	    public void FullName_testGetMiddleName_3() {
+	    	FullName name = new FullName("Sibel", "", "Demir");
+	    	assertEquals("", name.getMiddleName());
+	    }
+	    @Test
+	    public void FullName_testGetFamilyName() {
+	    	FullName name = new FullName("Ahmet", "Selami", "ŞAHİN");
+	    	assertEquals("ŞAHİN", name.getFamilyName());
+	    }
+	    
+	    @Test
+	    public void FullName_testFamilyName_2() {
+	    	FullName name = new FullName("Mehmet", "Eren", "ERBİL");
+	    	assertEquals("ERBİL", name.getFamilyName());
+	    }
+	    @Test
+	    public void FullName_testGetFamilyName_3() {
+	    	FullName name = new FullName("Sibel", "", "Demir");
+	    	assertEquals("Demir", name.getFamilyName());
+	    }
+	    @Test
+	    public void FullName_testSetFamilyName_1() {
+	    	FullName name = new FullName("Mehmet", "Eren", "ERBİL");
+	        name.setFamilyName("AYDIN");
+	        assertEquals("AYDIN",name.getFamilyName());
+	    }
+	    @Test
+	    public void FullName_testSetFamilyName_2() {
+	    	FullName name = new FullName("Sibel", "", "DEMİR");
+	        name.setFamilyName("ŞİŞMAN");
+	        assertEquals("ŞİŞMAN",name.getFamilyName());
+	    }
+	    @Test
+	    public void FullName_testSetFamilyName_3() {
+	    	FullName name = new FullName("Yagiz", "Sedat", "ŞİMŞEK");
+	        name.setFamilyName("ÇEKİRDEK");
+	        assertEquals("ÇEKİRDEK",name.getFamilyName());
+	    }
+	    @Test
+	    public void FullName_testSetFullName_1() {
+	    	FullName name = new FullName("Mehmet", "Eren", "ERBİL");
+	    	name.setGivenName("Polat");
+	    	name.setMiddleName("Can");
+	    	name.setFamilyName("ALEMDAR");
+	        assertEquals("Polat",name.getGivenName());
+	        assertEquals("Can",name.getMiddleName());
+	        assertEquals("ALEMDAR",name.getFamilyName());
+	    }
+	    @Test
+	    public void FullName_testSetFullName_2() {
+	    	FullName name = new FullName("Ömer", "Can", "Yıldız");
+	    	name.setGivenName("Mahmut");
+	    	name.setMiddleName("Erdem");
+	    	name.setFamilyName("Tuncer");
+	        assertEquals("Mahmut",name.getGivenName());
+	        assertEquals("Erdem",name.getMiddleName());
+	        assertEquals("Tuncer",name.getFamilyName());
+	    }
+	    @Test
+	    public void FullName_testSetFullName_3() {
+	    	FullName name = new FullName("Ali", "Ahmet", "Atasoy");
+	    	name.setGivenName("Sencer");
+	    	name.setMiddleName("Resul");
+	    	name.setFamilyName("Karahanlı");
+	        assertEquals("Sencer",name.getGivenName());
+	        assertEquals("Resul",name.getMiddleName());
+	        assertEquals("Karahanlı",name.getFamilyName());
+	    }
+
+	    @Test
+	    public void Phone_testGetPhoneNumber_1() {
+	    	Phone obj = new Phone(53146886);
+	        assertEquals(53146886,obj.getPhoneNumber());
+	    }
+	    @Test
+	    public void Phone_testGetPhoneNumber_2() {
+	    	Phone obj = new Phone(99846894);
+	        assertEquals(99846894,obj.getPhoneNumber());
+	    }
+	    @Test
+	    public void Phone_testGetPhoneNumber_3() {
+	    	Phone obj = new Phone(46259961);
+	        assertEquals(46259961,obj.getPhoneNumber());
+	    }
+	    @Test
+	    public void Phone_testSetPhoneNumber_1() {
+	    	Phone obj = new Phone(53146886);
+	        obj.setPhoneNumber(53346894);
+	        assertEquals(53346894,obj.getPhoneNumber());
+	    }
+	    @Test
+	    public void Phone_testSetPhoneNumber_2() {
+	    	Phone obj = new Phone(54146886);
+	        obj.setPhoneNumber(99846894);
+	        assertEquals(99846894,obj.getPhoneNumber());
+	    }
+	    @Test
+	    public void Phone_testSetPhoneNumber_3() {
+	    	Phone obj = new Phone(2336961);
+	        obj.setPhoneNumber(46259961);
+	        assertEquals(46259961,obj.getPhoneNumber());
+	    }
+	    
+	    @Test
+	    public void Hospital_testGetPhone_1() {
+	      Phone phone = new Phone(123456789);
+	      Hospital hospital = new Hospital("Farabi Hastanesi", phone, "Trabzon", "Ortahisar", "Hastane sok.", "No:1");
+	      assertEquals(phone, hospital.getPhone());
+	    }
+	    
+	    @Test
+	    public void Hospital_testSetPhone_1() {
+	        Phone phone = new Phone(5678901);
+	        phone.setPhoneNumber(46259961);
+	        Hospital hospital = new Hospital("Imperial Hastanesi", phone , "İstanbul", "Beşiktaş", "Barbaros Bulvarı", "No: 1");
+	        assertEquals(phone,hospital.getPhone());
+
+	    }
+	    
+	    @Test
+	    public void Hospital_testGetName_1() {
+	    	Phone phone = new Phone(53369161);
+	      Hospital hospital = new Hospital("Farabi Hastanesi", phone, "Trabzon", "Ortahisar", "Hastane sok.", "No:1");
+	      assertEquals("Farabi Hastanesi", hospital.getName());
+	    }
+	    
+	    @Test
+	    public void Hospital_testGetAddress_1() {
+	      Phone phone = new Phone(53369161);
+	      Address address = new Address("Trabzon", "Ortahisar", "Hastane Sok.", "No:1");
+	      Hospital hospital = new Hospital("Farabi Hastanesi", phone, "Trabzon", "Ortahisar", "Hastane sok.", "No:1");
+	      hospital.setAddress(address);
+	      assertEquals(address, hospital.getAddress());
+	    }
+	    
+	    @Test
+	    public void Hospital_testSetAddress_1() {
+	      Phone phone = new Phone(53369161);
+	      Address address = new Address("Trabzon", "Ortahisar", "Hastane Sok.", "No:1");
+	      Hospital hospital = new Hospital("Imperial Hastanesi", phone , "İstanbul", "Beşiktaş", "Barbaros Bulvarı", "No: 1");
+	      hospital.setAddress(address);
+	      assertEquals(address, hospital.getAddress());
+	    }
+	    
+	    @Test
+	    public void Hospital_testSetAddress_2() {
+	      Phone phone = new Phone(53369161);
+	      Address address = new Address("İstanbul", "Beşiktaş", "Cudibey Sok.", "No:5");
+	      Hospital hospital = new Hospital("Farabi Hastanesi", phone, "Trabzon", "Ortahisar", "Hastane sok.", "No:1");
+	      hospital.setAddress(address);
+	      assertEquals(address, hospital.getAddress());
+	    }
+	    
+	    @Test
+	    public void Gender_testSetMale_1() {
+	      Gender gender = new Gender("Male", "Female");
+	      gender.setMale("M");
+	      assertEquals("M", gender.getMale());
+	    }
+
+	    @Test
+	    public void Gender_testSetFemale_1() {
+	      Gender gender = new Gender("Male", "Female");
+	      gender.setFemale("F");
+	      assertEquals("F", gender.getFemale());
+	    }
+
+	    @Test
+	    public void Gender_testGetMale_1() {
+	      Gender gender = new Gender("Male", "Female");
+	      assertEquals("Male", gender.getMale());
+	    }
+
+	    @Test
+	    public void Gender_testGetFemale_1() {
+	      Gender gender = new Gender("Male", "Female");
+	      assertEquals("Female", gender.getFemale());
+	    }
+	    @Test
+	    public void Gender_testGetFemale_2() {
+	      Gender gender = new Gender("Man", "Woman");
+	      assertEquals("Woman", gender.getFemale());
+	    }
+	    
+	    @Test
+	    public void Department_testGetHospital_1() {
+	      Hospital hospital = new Hospital("Numune Hast.", new Phone(123456789), "Trabzon", "Ortahisar", "Erdoğdu", "No:5");
+	      Department department = new Department(hospital);
+	      assertEquals(hospital, department.getHospital());
+	    }
+
+	    @Test
+	    public void Department_testSetHospital_1() {
+	      Hospital hospital1 = new Hospital("Numune Hast.", new Phone(123456789), "Trabzon", "Ortahisar", "Erdoğdu", "No:5");
+	      Hospital hospital2 = new Hospital("Haçkalı Hast.", new Phone(123456789), "Trabzon", "Akçaabat", "101. Sok.", "No:8");
+	      Department department = new Department(hospital1);
+	      department.setHospital(hospital2);
+	      assertEquals(hospital2, department.getHospital());
+	    }
+	    @Test
+	    public void History_testSetSicknesses_1() {
+	      History history = new History("Flu");
+	      history.setSicknesses("Cold");
+	      assertEquals("Cold", history.getSicknesses());
+	    }
+
+	    @Test
+	    public void History_testGetSicknesses_1() {
+	      History history = new History("Flu");
+	      assertEquals("Flu", history.getSicknesses());
+	    }
+	    
+	    @Test
+	    public void testConstructor() {
+	      JuniorDoctor juniorDoctor = new JuniorDoctor();
+	      assertNotNull(juniorDoctor);
+	    }    
+
 
 }
