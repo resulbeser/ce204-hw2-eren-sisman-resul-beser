c40b5ce3c94db5c2e39811255fa2d0c57402998a
ERENSISMAN61 <eren_sisman20@erdogan.edu.tr>
2023-04-05 16:57:28 +0300

    test added

--- ce204_hw2_algo_lib/src/test/java/organization_test/OrganizationTest.java
+++ ce204_hw2_algo_lib/src/test/java/organization_test/OrganizationTest.java
@@ -1,14 +1,40 @@
 package organization_test;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
+import organization.Address;
+import organization.AdministrativeStaff;
+import organization.Department;
+import organization.Doctor;
+import organization.FrontDeskStaff;
+import organization.FullName;
+import organization.Gender;
+import organization.History;
+import organization.Hospital;
+import organization.Nurse;
+import organization.OperationsStaff;
+import organization.Patient;
+import organization.Person;
+import organization.Phone;
+import organization.Receptionist;
+import organization.Staff;
+import organization.Surgeon;
+import organization.SurgicalTechnologist;
+import organization.TechnicalStaff;
+import organization.Technician;
+import organization.Technologist;
+
+
 
 public class OrganizationTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testSetName() {
+		boolean wasSet = false;
+	    String name = "testName";
+	    wasSet = setName(name);
+	    assertTrue(wasSet);
+	
 	}
 
 }
--- ce204_hw2_algo_lib/src/test/java/treatment_test/TreatmentTest.java
+++ ce204_hw2_algo_lib/src/test/java/treatment_test/TreatmentTest.java
@@ -1,8 +1,15 @@
 package treatment_test;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
+import treatment.Diagnosis;
+import treatment.Doctor;
+import treatment.Examination;
+import treatment.Patient;
+import treatment.Prescription;
+import treatment.Surgery;
+import treatment.Therapy;
+import treatment.Treatment;
 
 public class TreatmentTest {
 
