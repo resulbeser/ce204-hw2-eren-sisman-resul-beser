047dafbef6370b809c4148136f3b692230c91f92
Resul Beser <93214337+resulbeser@users.noreply.github.com>
2023-04-05 16:24:12 +0300

    test added

---
+++ ce204_hw2_algo_lib/src/test/java/treatment_test/TreatmentTest.java
@@ -0,0 +1,14 @@
+package treatment_test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TreatmentTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ ce204_hw2_algo_lib/src/test/java/wards_and_teams_test/WardsAndTeamsTest.java
@@ -0,0 +1,14 @@
+package wards_and_teams_test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class WardsAndTeamsTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
